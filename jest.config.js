b155fed2fa9e3728efd01874e76615ec62ff5d45
fepompeu <felipe.pompeuu@gmail.com>
2022-01-20 00:38:13 -0300

    chore: disregard files in tests

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/server.ts',
+    '!<rootDir>/src/**/env.ts',
     '!<rootDir>/src/**/*-protocols.ts'
   ],
   coverageDirectory: 'coverage',
