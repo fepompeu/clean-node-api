79514fda868424a633887b1c7edc7ddf9a15a349
fepompeu <felipe.pompeuu@gmail.com>
2022-01-22 16:45:51 -0300

    test: ensure ValidationComposite returns the first error if more than one test fails

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -4,33 +4,47 @@ import { Validation } from './validator'
 
 interface SutTypes{
   sut: ValidationComposite
-  validationStub: Validation
+  validationStubs: Validation[]
 }
 
-const makeValidationStub = (): Validation => {
-  class ValidationStub implements Validation {
+const makeValidationStubs = (): Validation => {
+  class ValidationStubs implements Validation {
     validate (input: any): Error {
-      return new MissingParamError('field')
+      return null
     }
   }
-  return new ValidationStub()
+  return new ValidationStubs()
 }
 
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidationStub()
-  const sut = new ValidationComposite([validationStub])
+  const validationStubs = [
+    makeValidationStubs(),
+    makeValidationStubs()
+  ]
+  const sut = new ValidationComposite(validationStubs)
   return {
     sut,
-    validationStub
+    validationStubs
   }
 }
 
 describe('ValidationComposite', () => {
   test('should return an error if any validation fails', () => {
-    const { sut } = makeSut()
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
 
     const error = sut.validate({ field: 'any_value' })
 
     expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('should return the first error if more than one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toEqual(new Error())
   })
 })
