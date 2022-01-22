d64b95403fdfda91b2ffea9ce7848e1e290187b5
fepompeu <felipe.pompeuu@gmail.com>
2022-01-22 10:10:38 -0300

    refactor: refactor test

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,30 +2,39 @@ import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import { LoginController } from './login'
 
-const makeSut = (): LoginController => {
-  return new LoginController()
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return {
+    sut
+  }
 }
 
 describe('Login Controller', () => {
   test('should return 400 if no email is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
       }
     }
     const httpResponse = await sut.handle(httpRequest)
+
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
 
   test('should return 400 if no password is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_mail@mail.com'
       }
     }
     const httpResponse = await sut.handle(httpRequest)
+
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
 })
