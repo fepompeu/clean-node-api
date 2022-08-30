d067236070425aface7f7119a811c3ef90431508
fepompeu <felipe.pompeuu@gmail.com>
2022-08-29 22:11:25 -0300

    refactor: rename Encrypter to Hasher

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/add-account'
-export * from '../../protocols/cryptography/encypter'
+export * from '../../protocols/cryptography/hasher'
 export * from '../../protocols/db/add-account-repository'
