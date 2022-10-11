9c1b486e0c616a42c73f4ef7271d7c336c749a4f
fepompeu <felipe.pompeuu@gmail.com>
2022-10-10 23:30:16 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success and fails

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -22,6 +22,7 @@ export const MongoHelper = {
   },
 
   map: (collection: any): any => {
+    if (!collection) return undefined
     const { _id, ...collectionWithoutId } = collection
     return Object.assign({}, collectionWithoutId, { id: _id.toHexString() })
   }
