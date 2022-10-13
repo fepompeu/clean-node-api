a38c63737d9183ee3926e81a34383e5e48785d99
fepompeu <felipe.pompeuu@gmail.com>
2022-10-13 20:45:38 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 50550
+  port: process.env.PORT || 50550,
+  jwtSecret: process.env.JWT_SECRET || 'dsa21hu!@34123'
 }
